9e34b007c6d3fd02aaa3a32f7e0da23022a822a5
UsiFX <xprjkts@gmail.com>
2023-05-01 18:55:47 +0300

    translations: ar_AA: complete some UI translations
    Signed-off-by: UsiFX <xprjkts@gmail.com>

--- translations/ar_AA.ts
+++ translations/ar_AA.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ar">
 <context>
+    <name>Activity</name>
+    <message>
+        <location filename="../src/activity.cpp" line="166"/>
+        <source>Desktop</source>
+        <translation>سطح المكتب</translation>
+    </message>
+</context>
+<context>
     <name>ControlCenter</name>
     <message>
         <location filename="../qml/ControlCenter.qml" line="188"/>
@@ -30,6 +38,49 @@
         <location filename="../qml/ControlCenter.qml" line="213"/>
         <source>Dark Mode</source>
         <translation>الوضع الداكن</translation>
+    </message>
+    <message>
+      <location filename="../qml/ControlCenter.qml" line="173"/>
+      <source>Control Center<source/>
+      <translation>مركز التحكم<translation/>
+      </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="285"/>
+        <source>Do Not Disturb</source>
+        <translation>عدم الإزعاج</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="295"/>
+        <source>Screenshot</source>
+        <translation>لقطة شاشة</translation>
+    </message>
+</context>
+<context>
+    <name>ShutdownDialog</name>
+    <message>
+        <location filename="../qml/ShutdownDialog.qml" line="140"/>
+        <source>Shutdown</source>
+        <translatio>إيقاف التشغيل</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownDialog.qml" line="155"/>
+        <source>Reboot</source>
+        <translation>إعادة التشغيل</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownDialog.qml" line="170"/>
+        <source>Log out</source>
+        <translation>تسجيل خروج</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownDialog.qml" line="185"/>
+        <source>Lock Screen</source>
+        <translation>قفل الشاشة</translation>
+    </message>
+    <message>
+        <location filename="../qml/ShutdownDialog.qml" line="200"/>
+        <source>Suspend</source>
+        <translation>تعليق</translation>
     </message>
 </context>
 <context>
